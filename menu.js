5a35b24f8e1efa652648f3e10dea9019cc737d5d
shriya1123 <134806558+shriya1123@users.noreply.github.com>
2023-12-20 10:21:38 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,13 @@
+function showComponent(componentId) {
+    // Hide all menu components
+    var menuComponents = document.querySelectorAll('.menu-component');
+    menuComponents.forEach(function(component) {
+        component.style.display = 'none';
+    });
 
+    // Show the selected menu component
+    var selectedComponent = document.getElementById(componentId);
+    if (selectedComponent) {
+        selectedComponent.style.display = 'block';
+    }
+}
